c695437c4ab780771df9d57f057276ad92df3852
Jonatan Kruszewski <jonatan.kruszewski@justeattakeaway.com>
2025-03-16 07:51:08 +0200

    [FIX] Certifications - Improve ProjectTag

--- src/components/ProjectTag.tsx
+++ src/components/ProjectTag.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 
 type ProjectTagProps = {
-    name: string;
-    skillKey: string;
-    onClick: (name: string) => void;
+    label: string;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
     isSelected: boolean;
 };
 
-const ProjectTag = ({ name, onClick, isSelected, skillKey }: ProjectTagProps) => {
+const ProjectTag = ({ label, onClick, isSelected }: ProjectTagProps) => {
     const buttonStyles = isSelected
         ? "text-white border-primary-500"
         : "text-[#ADB7BE] border-slate-600 hover:border-white";
     return (
         <button
             className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
-            onClick={() => onClick(skillKey)}
+            onClick={onClick}
         >
-            {name}
+            {label}
         </button>
     );
 };
