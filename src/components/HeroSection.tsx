1d0448f1cccd2ff64fece2c35dce56769144cadf
Jonatan Kruszewski <jonatan.kruszewski@justeattakeaway.com>
2025-03-16 22:23:53 +0200

    [UI] Improvements

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -37,15 +37,44 @@ const HeroSection = () => {
                 />
                 <h2 className="text-gray-200 mt-8 mb-4 text-2xl sm:text-3xl lg:text-2xl lg:leading-normal font-extrabold">
                     I specialize in building scalable, maintainable front-end solutions that work seamlessly—without the
-                    tech debt nightmare. </h2>
-                <div className="flex items-center justify-center gap-4 py-4">
-                    <Link
-                        href="/#contact"
-                        className="px-6 inline-block py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
-                    >
-                        Contact Me
-                    </Link>
-                </div>
+                    tech debt nightmare.
+                </h2>
+
+
+                <Link
+                    href="/#contact"
+                    className="max-w-max border-1 border-transparent bg-gradient-to-br from-purple-400 to-indigo-500 p-[1px] rounded-full transition-all duration-300 ease-in-out "
+                >
+                    <div className="flex items-center gap-2 bg-[#121212] rounded-full px-8 py-4">
+                        <span className="bg-gradient-to-br from-purple-400 to-indigo-500 text-transparent bg-clip-text font-semibold text-lg">
+                            Get In Touch
+                        </span>
+
+                        {/* Gradient SVG */}
+                        <svg
+                            width="20"
+                            height="14"
+                            viewBox="0 0 20 14"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <defs>
+                                <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                                    <stop offset="0%" stopColor="#A855F7" /> {/* Purple-400 */}
+                                    <stop offset="100%" stopColor="#4F46E5" /> {/* Indigo-500 */}
+                                </linearGradient>
+                            </defs>
+
+                            {/* Apply Gradient to SVG Stroke */}
+                            <path
+                                d="M2 7H17M12.5 12.5L17.5 7L12.5 1.5"
+                                stroke="url(#gradient)"
+                                strokeWidth="2.4"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            />
+                        </svg>
+                    </div>
+                </Link>
 
             </div>
         </section>
