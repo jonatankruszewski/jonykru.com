6be699c4534f832598a6717258ebe0168024a581
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2025-03-16 21:48:36 +0200

    [FIX] Improve UI provider pill

--- src/components/ProjectTag.tsx
+++ src/components/ProjectTag.tsx
@@ -8,11 +8,12 @@ type ProjectTagProps = {
 
 const ProjectTag = ({ label, onClick, isSelected }: ProjectTagProps) => {
     const buttonStyles = isSelected
-        ? "text-white border-primary-500"
-        : "text-[#ADB7BE] border-slate-600 hover:border-white";
+        ? "text-white bg-primary-500 border-primary-500"
+        : "text-[#ADB7BE] border-slate-600 hover:border-white hover:text-white hover:bg-opacity-10";
+
     return (
         <button
-            className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+            className={`${buttonStyles} rounded-full border-2 px-4 py-2 text-sm md:px-6 md:py-3 md:text-xl cursor-pointer transition-all duration-300 ease-in-out`} // Adjusted padding and text size, added responsive modifiers, and transition
             onClick={onClick}
         >
             {label}
