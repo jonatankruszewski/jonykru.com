2fbeeca35a38f702941127253879cd21dcbea8ad
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2025-03-21 14:55:38 +0200

    [UI] Navbar improvements

--- src/components/NavbarLinks.tsx
+++ src/components/NavbarLinks.tsx
@@ -1,19 +1,24 @@
 import NavLink from "@/components/NavLink";
 import React from "react";
+import {CircleUser, Newspaper, ShieldCheck, UserPen} from 'lucide-react';
 
-const NavbarLinks = () => {
+type NavbarLinksProps = {
+    onClick?: () => void
+}
+
+const NavbarLinks = ({onClick}: NavbarLinksProps) => {
     return <>
         <li>
-            <NavLink href={"#about"} title="About"/>
+            <NavLink href={"#about"} title="About" icon={<CircleUser/>} onClick={onClick}/>
         </li>
         <li>
-            <NavLink href={"#publications"} title="Publications"/>
+            <NavLink href={"#publications"} title="Publications" icon={<Newspaper/>} onClick={onClick}/>
         </li>
         <li>
-            <NavLink href={"#certifications"} title="Certifications"/>
+            <NavLink href={"#certifications"} title="Certifications" icon={<ShieldCheck/>} onClick={onClick}/>
         </li>
         <li>
-            <NavLink href={"#contact"} title="Contact"/>
+            <NavLink href={"#contact"} title="Contact" icon={<UserPen/>} onClick={onClick}/>
         </li>
     </>
 }
