2ae741175a50d815e96db315c1ff93f9693b3496
Jonatan Kruszewski <jonatan.kruszewski@justeattakeaway.com>
2025-03-22 10:07:46 +0200

    [ACCESABILITY] Improved accesability

--- src/components/CertificationCard.tsx
+++ src/components/CertificationCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {transformCredlyUrl} from "@/utils/transformCredlyUrl";
 import {zigZagSort} from "@/utils/zigZagSort";
+import Link from "next/link";
 
 type CertificationCardProps = {
     badgeData: Record<string, unknown> & {
@@ -22,23 +23,24 @@ const CertificationCard: React.FC<CertificationCardProps> = (
     }) => {
 
     return (
-        <a href={previewUrl} target="_blank" rel="noopener noreferrer" className="overflow-hidden lg:p-6 block cursor-pointer text-white">
+        <li className="overflow-hidden lg:p-6 text-center flex flex-col gap-2">
             <img className="w-full max-w-[200px] mx-auto p-4" src={transformCredlyUrl(imgUrl)} alt={`${title} Badge`}/>
-            <h3 className="font-bold text-xl mb-5 text-center">
+            <Link href={previewUrl} target="_blank" rel="noopener noreferrer"
+               className="font-bold text-xl mb-2 text-center cursor-pointer text-white transition-opacity duration-200 hover:opacity-80 hover:underline">
                 {title}
-            </h3>
+            </Link>
             <div className="flex items-center flex-wrap justify-center">
                 {zigZagSort(badgeData.badge_template.skills)
                     .slice(0, 5)
                     .map((skill: string) => {
-                    return <p
-                        key={skill}
-                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                       {skill}
-                    </p>
-                })}
+                        return <p
+                            key={skill}
+                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                            {skill}
+                        </p>
+                    })}
             </div>
-        </a>
+        </li>
     );
 };
 
