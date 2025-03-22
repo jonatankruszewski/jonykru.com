cc2312557c78cdbbc0cc5fada79027eb216b6bec
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2025-03-22 09:56:08 +0200

    [FIX] Fix images, border

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     return (
         <nav>
             <div
-                className="border-1 bg-[#121212] bg-opacity-100 border-gray-600 flex w-full  z-40 fixed">
+                className="border-1 bg-[#121212] bg-opacity-100 border-b-gray-600 flex w-full  z-40 fixed">
                 <Disclosure as="div" className="flex w-full flex-col text-white mobile-menu justify-between">
                     {({open, close}) => (<DisclosureMenu open={open} close={close}/>)}
                 </Disclosure>
