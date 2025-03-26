00a7fc5740b9a5dc2965e46bcdd32f1f04a857ec
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2025-03-26 07:41:57 +0200

    [UI] Footer Improvements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-b-transparent border-r-transparent text-white">
-            <div className="p-3 flex flex-1 justify-between w-full">
-                <p className="text-slate-600">Copyright 2025</p>
-                <p className="text-slate-600">All rights reserved.</p>
+        <footer
+            className="footer border z-10 border-t-[#33353F] border-l-transparent border-b-transparent border-r-transparent">
+            <div className="p-3 flex flex-1 justify-between w-full container mx-auto px-4 md:px-6 lg:px-12 py-4 gap-20">
+                <p className="text-[#ADB7BE]">Copyright 2025</p>
+                <p className="text-[#ADB7BE]">All rights reserved.</p>
             </div>
         </footer>
     );
