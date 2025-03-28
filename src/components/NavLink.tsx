0641def6cee5a401315c8a956439336300ec0a3a
Jonatan Kruszewski <jonatan.kruszewski@justeattakeaway.com>
2025-03-28 17:27:28 +0300

    [WIP] Fix scroll in select navbar

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,5 +1,5 @@
+import type {LinkProps} from 'next/link'
 import Link from "next/link";
-import type { LinkProps } from 'next/link'
 import {ReactNode} from "react";
 import {FontJetBrainsMono} from "@/app/fonts";
 import {useSectionContext} from "@/context/sectionContext";
@@ -11,13 +11,18 @@ type NavLinkProps = {
     onClick?: () => void;
 } & LinkProps;
 
-const NavLink = ({ href, title, icon, onClick} : NavLinkProps) => {
-    const {visibleSection} = useSectionContext();
+const NavLink = ({href, title, icon, onClick}: NavLinkProps) => {
+    const {visibleSection, setVisibleSection} = useSectionContext();
     const selected = visibleSection === title;
 
     return (
         <Link
-            onClick={onClick}
+            onClick={() => {
+                setVisibleSection(title);
+                if (onClick) {
+                    onClick();
+                }
+            }}
             href={href}
             className={`${FontJetBrainsMono.className} py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl md:p-0 hover:text-white flex gap-2 font-extralight ${selected ? 'text-white' : ''}`}>
             {icon && icon}
