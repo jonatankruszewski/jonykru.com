6bb5fab9f55439e9a0150cd6575880e0e3569f4a
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2025-03-30 23:10:45 +0300

    WIP form

--- src/dataFetchers/medium.dataFetcher.tsx
+++ src/dataFetchers/medium.dataFetcher.tsx
@@ -1,12 +1,13 @@
 import {MediumData} from "@/types/medium.types";
-
+import mediumBackup from "@/dataFetchers/medium.backup.json"
 export const getMediumData = async () => {
     const res = await fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@jonakrusze", {
         next: { revalidate: 3600 },
     });
 
     if (!res.ok) {
-        throw new Error('Failed to fetch Medium')
+        return mediumBackup;
+        // throw new Error('Failed to fetch Medium')
     }
 
     return await res.json() as MediumData;
