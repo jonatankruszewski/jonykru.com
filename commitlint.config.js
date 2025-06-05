934d2861a52068973f9f2b253d1ff47ccd85dc8d
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2025-06-05 22:27:05 +0300

    [INFRA] Add commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,33 @@
+export default {
+  extends: [],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'REVERT',
+        'FIX',
+        'INFRA',
+        'UI',
+        'FEAT',
+        'CLEANUP',
+        'REFACTOR',
+        'UTIL',
+        'CONFIG'
+      ]
+    ],
+    'type-case': [2, 'always', 'upper-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-case': [0], // Disable subject case checking
+    'header-max-length': [2, 'always', 100],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always']
+  },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^\[(\w+)\]\s(.+)$/,
+      headerCorrespondence: ['type', 'subject']
+    }
+  }
+}
